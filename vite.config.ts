57bb1a65243994455c9488aed17bdcd65f942b1f
flavien <flavien@gensdeconfiance.com>
2024-12-09 18:27:11 +0100

    Fix deploy on gh pages

--- vite.config.ts
+++ vite.config.ts
@@ -1,10 +1,11 @@
 import path from "path";
 import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react";
-import svgr from 'vite-plugin-svgr'
+import svgr from "vite-plugin-svgr";
 
 // https://vitejs.dev/config/
 export default defineConfig({
+  base: "./",
   plugins: [react(), svgr()],
   resolve: {
     alias: {
